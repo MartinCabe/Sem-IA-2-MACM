de054f0964bf6e5a8a28c7c24aa8b7af0bbefc57
MartinCabe <martin.cabrera5980@alumnos.udg.mx>
2023-09-16 14:53:06 -0600

    Comentarios

--- Preparacion mlp/gradiente.py
+++ Preparacion mlp/gradiente.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math as m
 
 def gradienteDescendiente(f, fp, X, h, N):
+    # Crear el meshgrid para graficar
     x_lim = np.linspace(-5, 5, 50)
     y_lim = np.linspace(-5, 5, 50)
     x, y = np.meshgrid(x_lim, y_lim)
@@ -13,10 +13,13 @@ def gradienteDescendiente(f, fp, X, h, N):
     ax.plot_surface(x, y, z, cmap='viridis')
     
     for i in range(N):
+        # Se actualiza el vector con x e y con el descenso de gradiente
         X = X - h * fp(X[0], X[1])
 
+    # Imprimir resultado numerico
     print(f"x = {X[0]}, y = {X[1]}")
 
+    # Graficar resultados
     ax.scatter(X[0], X[1], f(X[0], X[1]), c='r', marker='*', s=100)
     
     ax.set_xlabel('x')
@@ -34,9 +37,19 @@ def gradienteDescendiente(f, fp, X, h, N):
 
     plt.show()
 
+# Funcion objetivo
 f = lambda x, y : 10 - np.exp(-(x**2 + 3*y**2))
+
+# Derivadas parciales
 fp = lambda x, y : np.array([2 * x * np.exp(-(x**2 + 3*y**2)), 6 * y * np.exp(-(x**2 + 3*y**2))])
+
+# Punto de inicio
 X = np.array([1, 2])
+
+# Tasa de aprendizaje
 h = 0.1
+
+# Iteraciones
 N = 100000
+
 gradienteDescendiente(f, fp, X, h, N)
